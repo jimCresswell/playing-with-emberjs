5afccdc6a9e2717f26854ddc62a2a0d48aeb41e6
jimCresswell <jim.cresswell@gmail.com>
2015-01-16 13:43:57 +0000

    Add more templaes and a corresponsing route.

--- js/app.js
+++ js/app.js
@@ -8,7 +8,10 @@
 
     var App = Ember.Application.create({
         LOG_TRANSITIONS: true,
+    });
 
+    App.Router.map(function() {
+        this.route('about', {path: '/aboutus'});
     });
 
 }(window.Ember));
