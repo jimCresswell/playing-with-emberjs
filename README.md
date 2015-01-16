4bc7f20f9ca8f1ac4e03cb55586c9e418ff71228
jimCresswell <jim.cresswell@gmail.com>
2015-01-16 09:27:57 +0000

    First commit. Adding Bower components and basic scaffolding.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Playing with Ember.js
+
+Following some Ember.js tutorials.
---
+++ js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Following an Ember.js tutorial.
+ */
+
+
+(function(Ember) {
+    'use strict';
+
+    var App = Ember.Application.create({});
+
+}(window.Ember));
